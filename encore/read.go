0e2858943891ac427b58b0d386ba2b6c1e5a2296
Han-Wen Nienhuys <hanwen@google.com>
2012-08-07 02:03:59 -0300

    Make read more private.

--- encore/read.go
+++ encore/read.go
@@ -9,17 +9,17 @@ import (
 	"strconv"
 )
 
-func (h *Header) FillFirstStaff(staff *Staff) {
+func (h *Header) fillFirstStaff(staff *Staff) {
 	raw := h.Raw[0xc2:]
 	staff.Raw = raw
 	staff.Offset = 0xc2
 	staff.VarData = raw[8:]
 	staff.VarSize = uint32(len(staff.VarData))
 
-	FillFields(raw[8:], &staff.StaffData)
+	fillFields(raw[8:], &staff.StaffData)
 }
 
-func (l *Line) ReadStaffs() {
+func (l *Line) readStaffs() {
 	d := l.VarData[26:]
 	if len(d)%30 != 0 {
 		log.Fatalf("must be multiple of 30: %d", len(d))
@@ -30,14 +30,14 @@ func (l *Line) ReadStaffs() {
 		d = d[30:]
 		i++
 		lsd := &LineStaffData{}
-		FillFields(staffRaw, lsd)
+		fillFields(staffRaw, lsd)
 		l.Staffs = append(l.Staffs, lsd)
 	}
 }
 
 func readElem(c []byte, off int) (result *MeasElem) {
 	result = new(MeasElem)
-	FillBlock(c, off, result)
+	fillBlock(c, off, result)
 	
 	var e MeasElemSpecific
 	switch result.Type() {
@@ -67,12 +67,12 @@ func readElem(c []byte, off int) (result *MeasElem) {
 	}
 
 	result.TypeSpecific = e
-	FillFields(c, e)
+	fillFields(c, e)
 	if result.Type() == TYPE_BEAM {
 		b := e.(*Beam)
 		b.SubBeams = make([]SubBeam, (result.Size - 14)/16)
 		for i := range b.SubBeams {
-			FillFields(result.Raw[14+16*i:], &b.SubBeams[i])
+			fillFields(result.Raw[14+16*i:], &b.SubBeams[i])
 		}
 	}
 	return result
@@ -80,7 +80,7 @@ func readElem(c []byte, off int) (result *MeasElem) {
 
 var endMarker = string([]byte{255, 255})
 
-func (m *Measure) ReadElems() {
+func (m *Measure) readElems() {
 	r := m.VarData
 	off := m.Offset + 62 // todo - extract.
 	for len(r) >= 3 {
@@ -102,8 +102,8 @@ func (m *Measure) ReadElems() {
 	}
 }
 
-// Like FillFields, but fill Raw/Offset too.
-func FillBlock(raw []byte, offset int, dest interface{}) {
+// Like fillFields, but fill Raw/Offset too.
+func fillBlock(raw []byte, offset int, dest interface{}) {
 	v := reflect.ValueOf(dest).Elem()
 	byteOffAddr := v.FieldByName("Offset").Addr().Interface().(*int)
 	*byteOffAddr = offset
@@ -111,14 +111,14 @@ func FillBlock(raw []byte, offset int, dest interface{}) {
 	rawAddr := v.FieldByName("Raw").Addr().Interface().(*[]byte)
 	*rawAddr = raw
 
-	FillFields(raw, dest)
+	fillFields(raw, dest)
 }
 
-func FillFields(raw []byte, dest interface{}) {
+func fillFields(raw []byte, dest interface{}) {
 	v := reflect.ValueOf(dest).Elem()
 	for i := 0; i < v.NumField(); i++ {
 		if v.Type().Field(i).Anonymous {
-			FillFields(raw, v.Field(i).Addr().Interface())
+			fillFields(raw, v.Field(i).Addr().Interface())
 			continue
 		}
 		f := v.Field(i)
@@ -137,7 +137,7 @@ func FillFields(raw []byte, dest interface{}) {
 	}
 }
 
-func ReadTaggedBlock(c []byte, off int, dest interface{}) int {
+func readTaggedBlock(c []byte, off int, dest interface{}) int {
 	v := reflect.ValueOf(dest).Elem()
 
 	tagField, ok := v.Type().FieldByName("Raw")
@@ -158,7 +158,7 @@ func ReadTaggedBlock(c []byte, off int, dest interface{}) int {
 	if string(raw[:len(want)]) != want {
 		log.Fatalf("Got tag %q want %q - %q", raw[:len(want)], want, raw)
 	}
-	FillBlock(raw, off, dest)
+	fillBlock(raw, off, dest)
 	return int(sz)
 }
 
@@ -172,26 +172,26 @@ func ReadData(c []byte) (*Data, error) {
 	f := new(Data)
 	f.Raw = c
 	off := 0
-	off += ReadTaggedBlock(c, off, &f.Header)
+	off += readTaggedBlock(c, off, &f.Header)
 	f.Staff = make([]*Staff, f.Header.StaffCount)
 	f.Staff[0] = new(Staff)
-	f.Header.FillFirstStaff(f.Staff[0])
+	f.Header.fillFirstStaff(f.Staff[0])
 	for i := 1; i < len(f.Staff); i++ {
 		s := new(Staff)
 		f.Staff[i] = s
 		s.Id = i
-		off += ReadTaggedBlock(c, off, s)
+		off += readTaggedBlock(c, off, s)
 		sz := int(s.VarSize) - 8
 		s.VarData = c[off : off+sz]
 		off += int(sz)
-		FillFields(s.VarData, &s.StaffData)
+		fillFields(s.VarData, &s.StaffData)
 	}
 
 	f.Pages = make([]*Page, f.Header.PageCount)
 	for i := 0; i < int(f.Header.PageCount); i++ {
 		p := new(Page)
 		p.Id = i
-		off += ReadTaggedBlock(c, off, p)
+		off += readTaggedBlock(c, off, p)
 		f.Pages[i] = p
 	}
 
@@ -200,11 +200,11 @@ func ReadData(c []byte) (*Data, error) {
 		l := new(Line)
 		l.Id = i
 		f.Lines[i] = l
-		off += ReadTaggedBlock(c, off, l)
+		off += readTaggedBlock(c, off, l)
 		l.VarData = c[off : off+int(l.VarSize)]
 		off += int(l.VarSize)
-		FillFields(l.VarData, &l.LineData)
-		l.ReadStaffs()
+		fillFields(l.VarData, &l.LineData)
+		l.readStaffs()
 	}
 
 	f.Measures = make([]*Measure, f.Header.MeasureCount)
@@ -212,10 +212,10 @@ func ReadData(c []byte) (*Data, error) {
 		m := new(Measure)
 		m.Id = i
 		f.Measures[i] = m
-		off += ReadTaggedBlock(c, off, m)
+		off += readTaggedBlock(c, off, m)
 		m.VarData = c[off : off+int(m.VarSize)]
 		off += int(m.VarSize)
-		m.ReadElems()
+		m.readElems()
 	}
 
 	setLinks(f)
